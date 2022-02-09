e382cd725d2d2f3b73aafccd0273791bc984f460
tremndez <steven482@live.com>
2022-02-09 10:20:12 -0500

    Removed cout

--- avlMain.cpp
+++ avlMain.cpp
@@ -13,7 +13,6 @@ int main()
     std::getline(std::cin, in);
     std::stringstream command;
     command << in;
-    std::cout << "Hello there" << std::endl;
     std::cout << command.str() << std::endl;
   }
   return 0;
